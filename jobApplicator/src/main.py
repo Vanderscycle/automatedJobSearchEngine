1d0514d1d2a1d2bdaf36b2742aa59256b5562c72
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-02-14 10:30:57 -0800

    created the jobApplicator folder and associated sudoCode.

---
+++ jobApplicator/src/main.py
@@ -0,0 +1,10 @@
+# sudo code
+# fetch data from the db
+# presents it in the cli
+# asks how many job you want to apply to today
+# check if you have applied to thet job already (whether that's not interreted or applied)
+# asks if you want to apply
+# - yes so opens a webpage to the job application, produces a cover letter, and updates the db to 
+# - no updates the database and mark it as not interrested
+# waits for your next command
+# packages cmd/rich/pymongo
